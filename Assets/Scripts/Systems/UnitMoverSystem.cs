8991bf9793f79daec58ff7b8831a6dc40d498bc0
Jess Salas <drengrdev@gmail.com>
2026-04-01 12:23:39 -0500

    Setup the UnitMoveSystem which is a query for some components that match exactly what the unit has, then uses it to move the unit. It now uses physics instead of moving it directly. Moves in fixed positiononly, will update.

--- Assets/Scripts/Systems/UnitMoverSystem.cs
+++ Assets/Scripts/Systems/UnitMoverSystem.cs
@@ -2,6 +2,7 @@ using Unity.Burst;
 using Unity.Entities;
 using Unity.Transforms;
 using Unity.Mathematics;
+using Unity.Physics;
 
 partial struct UnitMoverSystem : ISystem
 {
@@ -10,15 +11,24 @@ partial struct UnitMoverSystem : ISystem
     public void OnUpdate(ref SystemState state)
     {
         foreach((
-            RefRW<LocalTransform> localTransform, 
-            RefRO<MoveSpeed> MoveSpeed)
+            RefRW<LocalTransform> localTransform, //RW = Read/Write
+            RefRO<MoveSpeed> moveSpeed, //RO = Read Only
+            RefRW<PhysicsVelocity> physicsVelocity)
             in SystemAPI.Query<
                 RefRW<LocalTransform>,
-                RefRO<MoveSpeed>>()) //Query everything that has a LocalTransform and MoveSpeed
+                RefRO<MoveSpeed>,
+                RefRW<PhysicsVelocity>>()) //Query everything that has these components
         {
-            //Run on every single entity that has a localTransform and moveSpeed component
-            //Grabs localTransform, moveSpeed moving right by 1 unit
-            localTransform.ValueRW.Position = localTransform.ValueRO.Position + new float3(MoveSpeed.ValueRO.value, 0, 0) * SystemAPI.Time.DeltaTime;
+            //Run on every single entity that has a localTransform, moveSpeed, and PhysicsVelocity component
+            float3 targetPosition = localTransform.ValueRO.Position + new float3(10, 0, 0); //float3 is essentially equivalent to Vector3
+            float3 moveDirection = targetPosition - localTransform.ValueRO.Position;
+            moveDirection = math.normalize(moveDirection);
+
+            localTransform.ValueRW.Rotation = quaternion.LookRotation(moveDirection, math.up());//Think of it as vector3.up
+
+            physicsVelocity.ValueRW.Linear = moveDirection * moveSpeed.ValueRO.value;
+            physicsVelocity.ValueRW.Angular = float3.zero;
+            //localTransform.ValueRW.Position += moveDirection * moveSpeed.ValueRO.value * SystemAPI.Time.DeltaTime;
         }
     }
 
